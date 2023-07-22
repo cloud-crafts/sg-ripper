4fa13930d2416baf89c493268aff0d0bbcc1addf
Ernyoke <ernyoke19930115@gmail.com>
2023-07-23 00:49:23 +0300

    Refactor ec2 client.

---
+++ pkg/core/awsClients/ec2Client.go
@@ -0,0 +1,99 @@
+package awsClients
+
+import (
+	"context"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	ec2Types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+)
+
+const MaxResults = 1000
+
+type AwsEc2Client struct {
+	client *ec2.Client
+}
+
+func NewAwsEc2Client(cfg aws.Config) *AwsEc2Client {
+	return &AwsEc2Client{
+		client: ec2.NewFromConfig(cfg),
+	}
+}
+
+// DescribeSecurityGroups returns a list of Security Groups based on the list of Security Group IDs provided as an input
+func (c *AwsEc2Client) DescribeSecurityGroups(securityGroupIds []string) ([]ec2Types.SecurityGroup, error) {
+	filterName := "group-id"
+	var filters []ec2Types.Filter
+	if len(securityGroupIds) > 0 {
+		filters = append(filters, ec2Types.Filter{Name: &filterName, Values: securityGroupIds})
+	}
+
+	var nextToken *string = nil
+	securityGroups := make([]ec2Types.SecurityGroup, 0)
+	for {
+		sgResponse, err := c.client.DescribeSecurityGroups(context.TODO(),
+			&ec2.DescribeSecurityGroupsInput{
+				NextToken:  nextToken,
+				Filters:    filters,
+				MaxResults: aws.Int32(int32(MaxResults)),
+			})
+		if err != nil {
+			return nil, err
+		}
+		nextToken = sgResponse.NextToken
+		securityGroups = append(securityGroups, sgResponse.SecurityGroups...)
+
+		if nextToken == nil {
+			break
+		}
+	}
+
+	return securityGroups, nil
+}
+
+// DescribeSecurityGroupRules returns all the Security Group Rules. (TODO: try to optimise this to grab a sublist only)
+func (c *AwsEc2Client) DescribeSecurityGroupRules() ([]ec2Types.SecurityGroupRule, error) {
+	var nextToken *string = nil
+	securityGroupRules := make([]ec2Types.SecurityGroupRule, 0)
+	for {
+		sgResponse, err := c.client.DescribeSecurityGroupRules(context.TODO(),
+			&ec2.DescribeSecurityGroupRulesInput{NextToken: nextToken, MaxResults: aws.Int32(int32(MaxResults))})
+		if err != nil {
+			return nil, err
+		}
+		nextToken = sgResponse.NextToken
+		securityGroupRules = append(securityGroupRules, sgResponse.SecurityGroupRules...)
+
+		if nextToken == nil {
+			break
+		}
+	}
+
+	return securityGroupRules, nil
+}
+
+// DescribeNetworkInterfaces returns a list of Network Interfaces used by the security groups from the input slice
+func (c *AwsEc2Client) DescribeNetworkInterfaces(securityGroupIds []string) ([]ec2Types.NetworkInterface, error) {
+	filterName := "group-id"
+	var filters []ec2Types.Filter
+	if len(securityGroupIds) > 0 {
+		filters = append(filters, ec2Types.Filter{Name: &filterName, Values: securityGroupIds})
+	}
+
+	var nextToken *string = nil
+	networkInterfaces := make([]ec2Types.NetworkInterface, 0)
+	for {
+		ifcResponse, err := c.client.DescribeNetworkInterfaces(context.TODO(),
+			&ec2.DescribeNetworkInterfacesInput{NextToken: nextToken, MaxResults: aws.Int32(int32(MaxResults))})
+		if err != nil {
+			return nil, err
+		}
+
+		networkInterfaces = append(networkInterfaces, ifcResponse.NetworkInterfaces...)
+		nextToken = ifcResponse.NextToken
+
+		if nextToken == nil {
+			break
+		}
+	}
+	return networkInterfaces, nil
+}
--- pkg/core/sgUtils.go
+++ pkg/core/sgUtils.go
@@ -2,15 +2,11 @@ package core
 
 import (
 	"context"
-	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	ec2Types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
 	"sg-ripper/pkg/core/awsClients"
 	"sg-ripper/pkg/core/types"
 )
-
-const MaxResults = 1000
 
 const (
 	All SecurityGroupStatus = iota
@@ -28,31 +24,31 @@ type Filters struct {
 // If the slice is empty, all the security groups will be retrieved. Furthermore, we can apply filters to retrieved
 // Security Groups, for example: we can grab only the Security Groups which are in use or just unused ones.
 func ListSecurityGroups(securityGroupIds []string, filters Filters, region string, profile string) ([]types.SecurityGroup, error) {
-	cfg, configErr := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region), config.WithSharedConfigProfile(profile))
-	if configErr != nil {
-		return nil, configErr
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region), config.WithSharedConfigProfile(profile))
+	if err != nil {
+		return nil, err
 	}
 
-	ec2Client := ec2.NewFromConfig(cfg)
+	ec2Client := awsClients.NewAwsEc2Client(cfg)
+
+	securityGroups, err := ec2Client.DescribeSecurityGroups(securityGroupIds)
+	if err != nil {
+		return nil, err
+	}
+
+	securityGroupRules, err := ec2Client.DescribeSecurityGroupRules()
+	if err != nil {
+		return nil, err
+	}
+
+	networkInterfaces, err := ec2Client.DescribeNetworkInterfaces(securityGroupIds)
+	if err != nil {
+		return nil, err
+	}
 
 	awsLambdaClient := awsClients.NewAwsLambdaClient(cfg)
 	awsElbClient := awsClients.NewAwsElbClient(cfg)
 	ecsClient := awsClients.NewAwsEcsClient(cfg)
-
-	securityGroups, sgErr := describeSecurityGroups(ec2Client, securityGroupIds)
-	if sgErr != nil {
-		return nil, sgErr
-	}
-
-	securityGroupRules, sgRuleErr := describeSecurityGroupRules(ec2Client)
-	if sgRuleErr != nil {
-		return nil, sgRuleErr
-	}
-
-	networkInterfaces, ifcErr := describeNetworkInterfaces(ec2Client, securityGroupIds)
-	if ifcErr != nil {
-		return nil, ifcErr
-	}
 
 	nicCache := make(map[string]*types.NetworkInterface)
 
@@ -107,85 +103,6 @@ func ListSecurityGroups(securityGroupIds []string, filters Filters, region strin
 	}
 
 	return applyFilters(usage, filters), nil
-}
-
-// Get a list of Network Interfaces used by the security groups from the input slice
-func describeNetworkInterfaces(client *ec2.Client, securityGroupIds []string) ([]ec2Types.NetworkInterface, error) {
-	filterName := "group-id"
-	var filters []ec2Types.Filter
-	if len(securityGroupIds) > 0 {
-		filters = append(filters, ec2Types.Filter{Name: &filterName, Values: securityGroupIds})
-	}
-
-	var nextToken *string = nil
-	networkInterfaces := make([]ec2Types.NetworkInterface, 0)
-	for {
-		ifcResponse, err := client.DescribeNetworkInterfaces(context.TODO(),
-			&ec2.DescribeNetworkInterfacesInput{NextToken: nextToken, MaxResults: aws.Int32(int32(MaxResults))})
-		if err != nil {
-			return nil, err
-		}
-
-		networkInterfaces = append(networkInterfaces, ifcResponse.NetworkInterfaces...)
-		nextToken = ifcResponse.NextToken
-
-		if nextToken == nil {
-			break
-		}
-	}
-	return networkInterfaces, nil
-}
-
-// Get a list of Security Groups based on the list of Security Group IDs provided as an input
-func describeSecurityGroups(client *ec2.Client, securityGroupIds []string) ([]ec2Types.SecurityGroup, error) {
-	filterName := "group-id"
-	var filters []ec2Types.Filter
-	if len(securityGroupIds) > 0 {
-		filters = append(filters, ec2Types.Filter{Name: &filterName, Values: securityGroupIds})
-	}
-
-	var nextToken *string = nil
-	securityGroups := make([]ec2Types.SecurityGroup, 0)
-	for {
-		sgResponse, err := client.DescribeSecurityGroups(context.TODO(),
-			&ec2.DescribeSecurityGroupsInput{
-				NextToken:  nextToken,
-				Filters:    filters,
-				MaxResults: aws.Int32(int32(MaxResults)),
-			})
-		if err != nil {
-			return nil, err
-		}
-		nextToken = sgResponse.NextToken
-		securityGroups = append(securityGroups, sgResponse.SecurityGroups...)
-
-		if nextToken == nil {
-			break
-		}
-	}
-
-	return securityGroups, nil
-}
-
-// Get all the Security Group Rules. (TODO: try to optimise this to grab a sublist only)
-func describeSecurityGroupRules(client *ec2.Client) ([]ec2Types.SecurityGroupRule, error) {
-	var nextToken *string = nil
-	securityGroupRules := make([]ec2Types.SecurityGroupRule, 0)
-	for {
-		sgResponse, err := client.DescribeSecurityGroupRules(context.TODO(),
-			&ec2.DescribeSecurityGroupRulesInput{NextToken: nextToken, MaxResults: aws.Int32(int32(MaxResults))})
-		if err != nil {
-			return nil, err
-		}
-		nextToken = sgResponse.NextToken
-		securityGroupRules = append(securityGroupRules, sgResponse.SecurityGroupRules...)
-
-		if nextToken == nil {
-			break
-		}
-	}
-
-	return securityGroupRules, nil
 }
 
 // Get all the Network Interfaces which are associated to one of the Security Groups from the input list
